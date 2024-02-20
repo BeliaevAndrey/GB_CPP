8cbd2b4decefca87d8f168ebf03a863c5da52624
Andrei Beliaev <ab5030107021@yandex.ru>
2024-02-20 12:42:30 +0300

    lesson 11, task 04 updated

--- lesson_11/task04/task04.cpp
+++ lesson_11/task04/task04.cpp
@@ -22,26 +22,29 @@ int main()
     string checkBoard(string[]);
     string lines[3];
 
-    cout << "Введите результат игры\n" << endl;
+    cout << "Введите результат игры\n"
+         << endl;
     int i = 0;
-    for (; i < 3; i++){
-        cout << "строка " << i+1 << ":" << endl;
+    for (; i < 3; i++)
+    {
+        cout << "строка " << i + 1 << ":" << endl;
         cin >> lines[i];
-        if (!checkLine(lines[i])){
-                break;
-            }
+        if (!checkLine(lines[i]))
+        {
+            break;
+        }
     }
-    if (i < 3) {
+    if (i < 3)
+    {
         cout << "Incorrect" << endl;
     }
-    else if(checkBoard(lines) == "X")
+    else if (checkBoard(lines) == "X")
         cout << "X won" << endl;
-    else if(checkBoard(lines) == "O")
+    else if (checkBoard(lines) == "O")
         cout << "O won" << endl;
     else
         cout << "Nobody won" << endl;
     return 0;
-    
 }
 
 bool checkLine(string lineIn)
@@ -49,68 +52,97 @@ bool checkLine(string lineIn)
     for (int i = 0; i < lineIn.size(); i++)
         if (lineIn[i] != 'X' || lineIn[i] != 'O' || lineIn[i] != '.')
             return false;
+    return true;
 }
 
-string checkBoard(string lines[3]) {
+string checkBoard(string lines[3])
+{
     int countX = 0;
     int countO = 0;
     int countDot = 0;
     string checkLines(string[]);
     string checkColumns(string[]);
     string checkDiagonal(string[]);
-    if (checkLines(lines) == "X") return "X";
-    else if (checkLines(lines) == "O") return "O";
-    else if (checkColumns(lines) == "X") return "X";
-    else if (checkColumns(lines) == "O") return "O";
-    else if (checkDiagonal(lines) == "X") return "X";
-    else if (checkDiagonal(lines) == "O") return "O";
+    if (checkLines(lines) == "X")
+        return "X";
+    else if (checkLines(lines) == "O")
+        return "O";
+    else if (checkColumns(lines) == "X")
+        return "X";
+    else if (checkColumns(lines) == "O")
+        return "O";
+    else if (checkDiagonal(lines) == "X")
+        return "X";
+    else if (checkDiagonal(lines) == "O")
+        return "O";
 
     return ".";
 }
 
-string checkLines(string lines[3]) {
+string checkLines(string lines[3])
+{
     int countX = 0;
     int countO = 0;
-    for (int i=0; i < 3; i++) {
-        if (lines[i] == "XXX") return "X";
-        else if (lines[i] == "OOO") return "O";
+    for (int i = 0; i < 3; i++)
+    {
+        if (lines[i] == "XXX")
+            return "X";
+        else if (lines[i] == "OOO")
+            return "O";
     }
     return ".";
 }
 
-string checkColumns(string lines[3]) {
+string checkColumns(string lines[3])
+{
     int countX = 0;
     int countO = 0;
-    for (int i=0; i<3; i++){
-        for (int j=0; j < 3; j++){
-            if (lines[j][i] == 'X') countX++;
-            if (lines[j][i] == 'O') countO++;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (lines[j][i] == 'X')
+                countX++;
+            if (lines[j][i] == 'O')
+                countO++;
         }
-        if (countX == 3) return "X";
-        else if (countO == 3) return "O";
+        if (countX == 3)
+            return "X";
+        else if (countO == 3)
+            return "O";
         countX = 0;
         countO = 0;
     }
     return ".";
 }
 
-string checkDiagonal(string lines[3]) {
+string checkDiagonal(string lines[3])
+{
     int countX = 0;
     int countO = 0;
-    for (int i = 0; i < 3; i++) {
-        if (lines[i][i] == 'X') countX++;
-        else if (lines[i][i] == 'O') countO++;
+    for (int i = 0; i < 3; i++)
+    {
+        if (lines[i][i] == 'X')
+            countX++;
+        else if (lines[i][i] == 'O')
+            countO++;
     }
-    if (countX == 3) return "X";
-    else if (countO == 3) return "O";
+    if (countX == 3)
+        return "X";
+    else if (countO == 3)
+        return "O";
     countO = 0;
     countX = 0;
-    for (int i = 0, j = 2; i < 3, j >=0; i++, j--) {
-        if (lines[i][j] == 'X') countX++;
-        else if (lines[i][j] == 'O') countO++;
+    for (int i = 0, j = 2; i < 3, j >= 0; i++, j--)
+    {
+        if (lines[i][j] == 'X')
+            countX++;
+        else if (lines[i][j] == 'O')
+            countO++;
     }
-    if (countX == 3) return "X";
-    else if (countO == 3) return "O";
+    if (countX == 3)
+        return "X";
+    else if (countO == 3)
+        return "O";
     return ".";
-
 }
